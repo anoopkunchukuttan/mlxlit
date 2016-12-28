8d56fb117cf5509f189cb97618c421da09889b54
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2016-12-28 13:31:38 +0530

    CNN encoder implemented and tested

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 export MLXLIT_BASE=/home/development/anoop/experiments/multilingual_unsup_xlit
-export MLXLIT_HOME=$MLXLIT_BASE/src/multiling_unsup_xlit
+export MLXLIT_HOME=$MLXLIT_BASE/src/multiling_unsup_xlit_dev
 export XLIT_HOME=/home/development/anoop/experiments/unsupervised_transliterator/src/transliterator
 export PYTHONPATH=$PYTHONPATH:$MLXLIT_HOME/src:$XLIT_HOME/src 
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 #####################################################
 #################### LANGUAGE MODEL ################
@@ -119,71 +119,135 @@ export CUDA_VISIBLE_DEVICES=1
 #done     
 
 
-################### supervised transliteration #########################
+#################### supervised transliteration #########################
+#
+#data_dir=/home/development/anoop/experiments/multilingual_unsup_xlit/data/sup/conll16
+### refdir contains same ref as data_dir, but in XML format required for evaluation tools. These are from the CoNLL 2016 directories
+#ref_dir=~/experiments/unsupervised_transliterator/data/nonparallel/pb
+#output_dir=/home/development/anoop/experiments/multilingual_unsup_xlit/results/sup/conll16
+#
+##for expname in `echo 1_sup_nomono 2_bisup_nomono 3_sup_mono 4_bisup_mono`
+##for expname in `echo 1_sup_nomono 2_bisup_nomono 3_sup_mono 4_bisup_mono 3_2_use_src 3_3_use_tgt 4_2_all_loss 4_3_ll_rep_loss`
+#for expname in `echo 1_3_cnnencoder`
+#do 
+#
+#    exptype=`echo $expname | cut -f 1 -d '_'`
+#
+#    ######### choose specific model  ############
+#
+#    if [ $exptype = '1' ]  # sup_nomono
+#    then 
+#        train_bidirectional=''
+#        use_monolingual=''
+#    elif [ $exptype = '2' ] # bisup_nomono 
+#    then 
+#        train_bidirectional='--train_bidirectional'
+#        use_monolingual=''
+#    elif [ $exptype = '3' ] # sup_mono 
+#    then 
+#        train_bidirectional=''
+#        use_monolingual='--use_monolingual'
+#    elif [ $exptype = '4' ] # bisup_mono
+#    then 
+#        train_bidirectional='--train_bidirectional'
+#        use_monolingual='--use_monolingual'
+#    fi 
+#   
+#    ######## Experiment loop starts here ########
+#
+#    #for langpair in `echo hi-kn bn-hi ta-kn`
+#    for langpair in `echo hi-kn`
+#    do
+#        src_lang=`echo $langpair | cut -f 1 -d '-'`
+#        tgt_lang=`echo $langpair | cut -f 2 -d '-'`
+#    
+#        #for representation in `echo onehot phonetic`
+#        for representation in `echo phonetic`
+#        do 
+#            o=$output_dir/$expname/$representation/$langpair
+#            
+#            echo 'Start: ' $expname $langpair $representation 
+#    
+#            ## Training and Testing 
+#            #rm -rf $o
+#            #mkdir -p $o
+#            #python $MLXLIT_HOME/src/unsup_xlit/ModelTraining.py \
+#            #    --train_mode sup \
+#            #    $train_bidirectional \
+#            #    $use_monolingual \
+#            #    --lang_pairs "$src_lang-$tgt_lang" \
+#            #    --data_dir  $data_dir/$langpair \
+#            #    --output_dir  $o \
+#            #    --start_from $o/final_model_epochs_?? \
+#            #    --representation $representation \
+#            #    --max_epochs 100 >> $o/train.log 2>&1 
+#    
+#            #### Evaluation starts 
+#            
+#            resdir=final_output
+#            prefix=`ls $o/$resdir/ | sed 's,[^0-9],,g' | sort -r -n | head -1`
+#    
+#            ## convert to required format 
+#            python utilities.py convert_output_format  \
+#                $o/${resdir}/${prefix}${src_lang}-${tgt_lang}_ \
+#                $o/${resdir}/${prefix}test.${tgt_lang} 
+#    
+#            # convert to n-best format 
+#            python $XLIT_HOME/src/cfilt/transliteration/news2015_utilities.py  convert_to_nbest_format  \
+#                $o/$resdir/${prefix}test.${tgt_lang}  $o/$resdir/${prefix}test.nbest.${tgt_lang}
+#            
+#            # generate NEWS 2015 evaluation format output file 
+#            python $XLIT_HOME/src/cfilt/transliteration/news2015_utilities.py gen_news_output \
+#                    "$ref_dir/$src_lang-$tgt_lang/test.id" \
+#                    "$ref_dir/$src_lang-$tgt_lang/test.xml" \
+#                    "$o/$resdir/${prefix}test.nbest.${tgt_lang}" \
+#                    "$o/$resdir/${prefix}test.nbest.${tgt_lang}.xml" \
+#                    "system" "conll2016" "$src_lang" "$tgt_lang"  
+#            
+#            # run evaluation 
+#            python $XLIT_HOME/scripts/news_evaluation_script/news_evaluation.py \
+#                    -t "$ref_dir/$src_lang-$tgt_lang/test.xml" \
+#                    -i "$o/$resdir/${prefix}test.nbest.${tgt_lang}.xml" \
+#                    -o "$o/$resdir/${prefix}test.nbest.${tgt_lang}.detaileval.csv" \
+#                     > "$o/$resdir/${prefix}test.nbest.${tgt_lang}.eval"
+#    
+#            echo 'End: ' $expname $langpair $representation 
+#    
+#        done 
+#    done     
+#
+#
+#done 
 
-data_dir=/home/development/anoop/experiments/multilingual_unsup_xlit/data/sup/conll16
+########### EVALUATE SPECIFIC ITERATION #################
+
 ## refdir contains same ref as data_dir, but in XML format required for evaluation tools. These are from the CoNLL 2016 directories
 ref_dir=~/experiments/unsupervised_transliterator/data/nonparallel/pb
 output_dir=/home/development/anoop/experiments/multilingual_unsup_xlit/results/sup/conll16
 
-#for expname in `echo 1_sup_nomono 2_bisup_nomono 3_sup_mono 4_bisup_mono`
-#for expname in `echo 1_sup_nomono 2_bisup_nomono 3_sup_mono 4_bisup_mono 3_2_use_src 3_3_use_tgt 4_2_all_loss 4_3_ll_rep_loss`
-for expname in `echo 1_2_attention_2`
+prefix=016
+
+for expname in `echo 1_3_cnnencoder`
 do 
 
-    exptype=`echo $expname | cut -f 1 -d '_'`
-
-    ######### choose specific model  ############
-
-    if [ $exptype = '1' ]  # sup_nomono
-    then 
-        train_bidirectional=''
-        use_monolingual=''
-    elif [ $exptype = '2' ] # bisup_nomono 
-    then 
-        train_bidirectional='--train_bidirectional'
-        use_monolingual=''
-    elif [ $exptype = '3' ] # sup_mono 
-    then 
-        train_bidirectional=''
-        use_monolingual='--use_monolingual'
-    elif [ $exptype = '4' ] # bisup_mono
-    then 
-        train_bidirectional='--train_bidirectional'
-        use_monolingual='--use_monolingual'
-    fi 
-   
     ######## Experiment loop starts here ########
 
     #for langpair in `echo hi-kn bn-hi ta-kn`
-    for langpair in `echo ta-kn`
+    for langpair in `echo hi-kn`
     do
         src_lang=`echo $langpair | cut -f 1 -d '-'`
         tgt_lang=`echo $langpair | cut -f 2 -d '-'`
     
         #for representation in `echo onehot phonetic`
-        for representation in `echo onehot`
+        for representation in `echo phonetic`
         do 
             o=$output_dir/$expname/$representation/$langpair
             
             echo 'Start: ' $expname $langpair $representation 
     
-            ## Training and Testing 
-            rm -rf $o
-            mkdir -p $o
-            python $MLXLIT_HOME/src/unsup_xlit/ModelTraining.py \
-                --train_mode sup \
-                $train_bidirectional \
-                $use_monolingual \
-                --lang_pairs "$src_lang-$tgt_lang" \
-                --data_dir  $data_dir/$langpair \
-                --output_dir  $o \
-                --representation $representation > $o/train.log 2>&1 
-    
             #### Evaluation starts 
             
-            resdir=final_output
-            prefix=`ls $o/$resdir/ | sed 's,[^0-9],,g' | sort -r -n | head -1`
+            resdir=outputs
     
             ## convert to required format 
             python utilities.py convert_output_format  \
@@ -213,7 +277,6 @@ do
     
         done 
     done     
-
 
 done 
 
--- src/unsup_xlit/AttentionModel.py
+++ src/unsup_xlit/AttentionModel.py
@@ -23,7 +23,7 @@ class Encoder(object):
 
 class SimpleRnnEncoder(Encoder):
 
-    def __init__(self,embedding_size,rnn_size,max_sequence_length):
+    def __init__(self,embedding_size,max_sequence_length,rnn_size):
         self.embedding_size=embedding_size
         self.rnn_size=rnn_size 
         self.max_sequence_length=max_sequence_length 
@@ -44,7 +44,7 @@ class SimpleRnnEncoder(Encoder):
 
 class BidirectionalRnnEncoder(Encoder):
 
-    def __init__(self,embedding_size,rnn_size,max_sequence_length):
+    def __init__(self,embedding_size,max_sequence_length,rnn_size):
         self.embedding_size=embedding_size
         self.rnn_size=rnn_size 
         self.max_sequence_length=max_sequence_length 
@@ -65,39 +65,71 @@ class BidirectionalRnnEncoder(Encoder):
     def get_state_size(self): 
         return self.fw_encoder_cell.state_size 
 
-#class CNNEncoder(Encoder):
-#
-#    def __init__(self,embedding_size,rnn_size,max_sequence_length):
-#        pass 
-#
-#    def encode(self, sequence_embeddings, sequence_lengths):
-#        input_data = tf.expand_dims(sequence_embeddings, -1)
-#
-#        # Create a convolution + maxpool layer for each filter size
-#        pooled_outputs = []
-#        for i, filter_size in enumerate(self.filter_sizes):
-#            with tf.name_scope("conv-maxpool-%s" % filter_size):
-#                # Convolution Layer
-#                filter_shape = [filter_size, self.embedding_size, 1, self.num_filters]
-#                W = tf.Variable(tf.random_uniform(filter_shape, [-0.1, 0.1]), name="W")
-#                b = tf.Variable(tf.constant(0.1, shape=[self.num_filters]), name="b")
-#                conv = tf.nn.conv2d(
-#                    input_data,
-#                    W,
-#                    strides=[1, 1, 1, 1],
-#                    padding="VALID",
-#                    name="conv")
-#                # Apply nonlinearity
-#                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-#                # Maxpooling over the outputs
-#                pooled = tf.nn.max_pool(
-#                    h,
-#                    ksize=[1, self.max_sequence_length - filter_size + 1, 1, 1],
-#                     
-#                    strides=[1, 1, 1, 1],
-#                    padding='VALID',
-#                    name="pool")
-#                pooled_outputs.append(pooled)
+class CNNEncoder(Encoder):
+
+    def __init__(self,embedding_size,max_sequence_length,filter_sizes,num_filters):
+        self.filter_sizes=filter_sizes
+        self.embedding_size=embedding_size
+        self.num_filters=num_filters
+        self.max_sequence_length=max_sequence_length 
+
+        self.W=[]
+        self.b=[]
+        for i, filter_size in enumerate(self.filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
+                filter_shape = [filter_size, self.embedding_size, 1, self.num_filters]
+                self.W.append(tf.Variable(tf.random_uniform(filter_shape, -0.1, 0.1), name="W"))
+                self.b.append(tf.Variable(tf.constant(0.0, shape=[self.num_filters]), name="b"))
+
+    def encode(self, sequence_embeddings, sequence_lengths):
+        input_data = tf.expand_dims(sequence_embeddings, -1)
+
+        # Create a convolution + maxpool layer for each filter size
+        pooled_outputs = []
+        for i, filter_size in enumerate(self.filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
+                conv = tf.nn.conv2d(
+                    input_data,
+                    self.W[i],
+                    strides=[1, 1, self.embedding_size, 1],
+                    padding="SAME",
+                    name="conv")
+                # Apply nonlinearity
+                h = tf.nn.relu(tf.nn.bias_add(conv, self.b[i]), name="relu")
+                # Maxpooling over the outputs
+                pooled = tf.nn.max_pool(
+                    h,
+                    ksize=[1, self.max_sequence_length, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='SAME',
+                    name="pool")
+                pooled_outputs.append(pooled)
+
+        ## encoder output matrix of dimension: [batch,max_sequence_length,len(filter_sizes)*num_filters]
+        total_num_filters=self.num_filters*len(self.filter_sizes)
+        enc_output_matrix=tf.reshape(tf.squeeze(tf.concat(3,pooled_outputs)),
+                [-1,self.max_sequence_length,total_num_filters])
+
+        ## output encoding 
+        enc_outputs=tf.unpack(tf.transpose(enc_output_matrix,[1,0,2]))
+
+        ## final state generation: taking as average of all time step vectors
+        def state_gen_func(batch_no): 
+            s=tf.slice(enc_output_matrix,[batch_no,0,0],[1,self.max_sequence_length,self.get_output_size()])
+            return tf.add_n(tf.unpack(tf.squeeze(s)))/self.max_sequence_length
+
+        batch_size=tf.shape(enc_output_matrix)[0]
+        states=tf.map_fn(state_gen_func,tf.range(0,batch_size),dtype=tf.float32)
+
+        return states, enc_outputs 
+
+    def get_output_size(self): 
+        return self.num_filters*len(self.filter_sizes)
+
+    def get_state_size(self): 
+        return self.num_filters*len(self.filter_sizes)
 
 # Do something with output folder
 class AttentionModel():
@@ -127,9 +159,14 @@ class AttentionModel():
             self.embed_W[lang] = tf.matmul(self.bitvector_embeddings[lang], self.embed_W0, name='embed_W_{}'.format(lang))
         self.embed_b = tf.Variable(tf.constant(0., shape=[embedding_size]), name = 'embed_b')
 
-        # Creating BasicLSTM Cells and initial inputs to decoders
-        ## Encoder
-        self.input_encoder=BidirectionalRnnEncoder(embedding_size,rnn_size,max_sequence_length)
+        ##### Create Encoder
+
+        ## Bidirectional RNN encoder 
+        #self.input_encoder=BidirectionalRnnEncoder(embedding_size,max_sequence_length,rnn_size)
+
+        ## CNN Encoder
+        filter_sizes=[1,2,3,4]
+        self.input_encoder=CNNEncoder(embedding_size,max_sequence_length,filter_sizes,rnn_size*2/len(filter_sizes))
 
         ## FIXME: what is the best way to initialize the input - I suppose with embedding for GO symbol
         ## the variable need not even be saved
