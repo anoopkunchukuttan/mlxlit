0ec5bbe8ae15d548ac1cd052d6fe8d2569bae134
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2017-02-12 14:38:54 +0530

    Fix error in loading of vocabulary

--- src/unsup_xlit/Mapping.py
+++ src/unsup_xlit/Mapping.py
@@ -54,137 +54,6 @@ class Mapping():
     def get_words_from_id_lists(self,sequences,lang):
             return [self.get_word_from_ids(sequence, lang) for sequence in sequences]
 
-#class IndicPhonetic2Mapping(Mapping):
-#
-#    def __init__(self): 
-#
-#        ### members
-#        self.addvocab_c2i=defaultdict(lambda: li.COORDINATED_RANGE_END_INCLUSIVE + 1 + len(self.addvocab_c2i))
-#        self.addvocab_i2c={}
-#        self.lang_list=set()
-#        
-#        ## state members 
-#        self.update_mode=True
-#
-#        ## add standard vocabulary 
-#        self.addvocab_c2i[Mapping.GO]
-#        self.addvocab_c2i[Mapping.EOW]
-#        self.addvocab_c2i[Mapping.PAD]
-#        self.addvocab_c2i[Mapping.UNK]
-#
-#    def save_mapping(self,mapping_file): 
-#        dump_data={}
-#        dump_data['c2i']=self.addvocab_c2i
-#        dump_data['i2c']=self.addvocab_i2c
-#        dump_data['langs']=list(self.lang_list)
-#        json.dump(dump_data,mapping_file)
-#
-#    def load_mapping(self,mapping_file): 
-#        dump_data=json.load(mapping_file)
-#        self.addvocab_c2i=dump_data['c2i']
-#        self.addvocab_i2c=dump_data['i2c']
-#        self.lang_list=set(dump_data['langs'])
-#        self.update_mode=False
-#
-#    def finalize_vocab(self): 
-#        """
-#        Call after all vocabulary has been added via get_index
-#        """
-#        if self.update_mode: 
-#            for c,i in self.addvocab_c2i.iteritems(): 
-#                self.addvocab_i2c[i]=c
-#            self.update_mode=False
-#
-#    def get_index(self,c,lang=None): 
-#        if len(c)==1 and isc.in_coordinated_range(c,lang): 
-#            index=isc.get_offset(c,lang)
-#        else:
-#            if (not self.update_mode) and (c not in self.addvocab_c2i): 
-#                c=Mapping.UNK
-#            index=self.addvocab_c2i[c]
-#
-#        if self.update_mode: 
-#            self.lang_list.add(lang)
-#
-#        return index
-#
-#    def get_char(self,index,lang): 
-#        if isc.in_coordinated_range_offset(index): 
-#            c=isc.offset_to_char(index,lang)
-#        else: 
-#            c=self.addvocab_i2c.get(index,Mapping.UNK)
-#        return c
-#
-#    def get_langs(self): 
-#        return self.lang_list 
-#
-#    def get_vocab_size(self): 
-#        return (li.COORDINATED_RANGE_END_INCLUSIVE + 1 + len(self.addvocab_c2i))
-#
-#    def get_vocab(self,lang): 
-#        return [ self.get_char(i,lang) for i in range(0,self.get_vocab_size()) ]
-#
-#    def get_bitvector_embedding_size(self,representation='phonetic'): 
-#        if representation=='phonetic':
-#            return isc.PHONETIC_VECTOR_LENGTH+len(self.addvocab_c2i)
-#        elif representation  in ['onehot','onehot_shared']: 
-#            return self.get_vocab_size()
-#        elif representation=='onehot_and_phonetic':
-#            return self.get_vocab_size()+isc.PHONETIC_VECTOR_LENGTH  ### FIXME: this size is wrong
-#
-#    def get_phonetic_bitvector_embeddings(self,lang):
-#        """
-#        Create bit-vector embeddings for vocabulary items. For phonetic chars,
-#        use phonetic embeddings, else use 1-hot embeddings
-#        """
-#    
-#        ##  phonetic embeddings for basic characters 
-#        pv=isc.get_phonetic_info(lang)[1]
-#    
-#        ## vocab statistics
-#        pv=np.copy(pv)
-#        org_shape=pv.shape
-#        additional_vocab=self.get_vocab_size()-org_shape[0]
-#    
-#        ##  new rows added 
-#        new_rows=np.zeros([additional_vocab,pv.shape[1]])
-#        pv=np.concatenate([pv,new_rows])
-#    
-#        ##  new columns added 
-#        new_cols=np.zeros([pv.shape[0],additional_vocab])
-#        pv=np.concatenate([pv,new_cols],axis=1)
-#    
-#        assert( (pv.shape[0]-org_shape[0]) == (pv.shape[1]-org_shape[1]) )
-#    
-#        ## 1-hot embeddings for new characters 
-#        for j,k in zip(range(org_shape[0],pv.shape[0]),range(org_shape[1],pv.shape[1])): 
-#            pv[j,k]=1
-#    
-#        return pv
-#    
-#    def get_onehot_bitvector_embeddings(self,lang): 
-#        return np.identity(self.get_vocab_size())
-#    
-#    def get_onehot_phonetic_bitvector_embeddings(self,lang): 
-#        """
-#        Concatenation of the one-hot and phonetic represenation
-#        For vocabulary items which don't have a phonetic representation, they are represented in the one-hot
-#        component. So, the phonetic component does not have the onehot subcomponent to avoid onehot duplication
-#        (unlike the call in get_phonetic_bitvector_embeddings)
-#        """
-#        phv=self.get_phonetic_bitvector_embeddings(lang)[:,:isc.PHONETIC_VECTOR_LENGTH]
-#        ohv=np.identity(self.get_vocab_size())
-#        return np.concatenate([ohv,phv],1)
-#
-#    def get_bitvector_embeddings(self,lang,representation='phonetic'): 
-#    
-#        if representation=='phonetic':
-#            return self.get_phonetic_bitvector_embeddings(lang)
-#        elif representation  in ['onehot','onehot_shared']: 
-#            return self.get_onehot_bitvector_embeddings(lang)
-#        elif representation=='onehot_and_phonetic': 
-#            return self.get_onehot_phonetic_bitvector_embeddings(lang)
-
 class IndicPhoneticMapping(Mapping):
 
     def __init__(self): 
@@ -216,8 +85,15 @@ class IndicPhoneticMapping(Mapping):
     def load_mapping(self,mapping_file): 
         dump_data=json.load(mapping_file)
         self.vocab_c2i=dump_data['c2i']
-        self.vocab_i2c=dump_data['i2c']
-        self.indic_i2pid=dump_data['i2pid']
+
+        self.vocab_i2c={}
+        for i_str,c in dump_data['i2c'].iteritems(): 
+            self.vocab_i2c[int(i_str)]=dump_data['i2c'][i_str]
+
+        self.indic_i2pid={}
+        for i_str,pid in dump_data['i2pid'].iteritems(): 
+            self.indic_i2pid[int(i_str)]=dump_data['i2pid'][i_str]
+
         self.lang_list=set(dump_data['langs'])
         self.update_mode=False
 
@@ -285,6 +161,7 @@ class IndicPhoneticMapping(Mapping):
         """
 
         non_phonetic_chars=filter(lambda x: x not in self.indic_i2pid, range(self.get_vocab_size()))
+        print 'non_phonetic_chars: {}'.format(len(non_phonetic_chars))
         bitvector_embedding=np.zeros((self.get_vocab_size(), isc.PHONETIC_VECTOR_LENGTH+len(non_phonetic_chars)))
 
         for p,index in enumerate(non_phonetic_chars): 
@@ -293,6 +170,7 @@ class IndicPhoneticMapping(Mapping):
         for index in self.indic_i2pid.keys(): 
             bitvector_embedding[index,:isc.PHONETIC_VECTOR_LENGTH]=isc.get_phonetic_feature_vector(self.get_char(index,lang),lang)
         
+        print bitvector_embedding.shape
         return bitvector_embedding
 
     def get_onehot_bitvector_embeddings(self,lang): 
@@ -338,6 +216,7 @@ class CharacterMapping(Mapping):
 
     def save_mapping(self,mapping_file): 
         dump_data={}
+        dump_data['class']='CharacterMapping'
         dump_data['c2i']=self.vocab_c2i
         dump_data['i2c']=self.vocab_i2c
         dump_data['langs']=list(self.lang_list)
@@ -346,7 +225,11 @@ class CharacterMapping(Mapping):
     def load_mapping(self,mapping_file): 
         dump_data=json.load(mapping_file)
         self.vocab_c2i=dump_data['c2i']
-        self.vocab_i2c=dump_data['i2c']
+
+        self.vocab_i2c={}
+        for i_str,c in dump_data['i2c'].iteritems(): 
+            self.vocab_i2c[int(i_str)]=dump_data['i2c'][i_str]
+
         self.lang_list=set(dump_data['langs'])
         self.update_mode=False
 
